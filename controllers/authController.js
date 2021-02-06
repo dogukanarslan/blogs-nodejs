79c99008160f998fc61b9304e2dfa5187f966fed
dogukanarslan <dogukan2arslan@hotmail.com>
2021-02-06 16:46:31 +0300

    Create token on signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User.js');
+const jwt = require('jsonwebtoken');
 
 // Handle errors
 const handleErrors = (err) => {
@@ -13,12 +14,19 @@ const handleErrors = (err) => {
 
   // Validation error
   if (err.message.includes('user validation failed')) {
-    Object.values(err.errors).forEach(({properties}) => {
+    Object.values(err.errors).forEach(({ properties }) => {
       errors[properties.path] = properties.message;
-    })
+    });
   }
 
   return errors;
+};
+
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+  return jwt.sign({id}, 'themostsecurethingintheworld', {
+    expiresIn: maxAge
+  })
 };
 
 module.exports.signup_get = (req, res) => {
@@ -34,7 +42,9 @@ module.exports.signup_post = async (req, res) => {
 
   try {
     const user = await User.create({ email, password });
-    res.status(201).json(user);
+    const token = createToken(user._id);
+    res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge * 1000});
+    res.status(201).json({user: user._id});
   } catch (err) {
     const errors = handleErrors(err);
     res.status(400).json(errors);
