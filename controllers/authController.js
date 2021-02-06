95e38ebea745e42b43b6d76c361098271020a976
dogukanarslan <dogukan2arslan@hotmail.com>
2021-02-06 13:03:16 +0300

    Validate on signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,47 @@
+const User = require('../models/User.js');
+
+// Handle errors
+const handleErrors = (err) => {
+  console.log(err.message, err.code);
+
+  let errors = { email: '', password: '' };
+
+  if (11000 === err.code) {
+    errors.email = 'That email is already registered';
+    return errors;
+  }
+
+  // Validation error
+  if (err.message.includes('user validation failed')) {
+    Object.values(err.errors).forEach(({properties}) => {
+      errors[properties.path] = properties.message;
+    })
+  }
+
+  return errors;
+};
+
 module.exports.signup_get = (req, res) => {
-    res.render('auth/signup', {title: 'Sign up!'});
+  res.render('auth/signup', { title: 'Sign up!' });
 };
 
 module.exports.login_get = (req, res) => {
-    res.render('auth/login', {title: 'Login!'});
+  res.render('auth/login', { title: 'Login!' });
 };
 
-module.exports.signup_post = (req, res) => {
-    res.send('New signup');
+module.exports.signup_post = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.create({ email, password });
+    res.status(201).json(user);
+  } catch (err) {
+    const errors = handleErrors(err);
+    res.status(400).json(errors);
+  }
+  res.send('New signup');
 };
 
 module.exports.login_post = (req, res) => {
-    res.send('Login post');
+  res.send('Login post');
 };
