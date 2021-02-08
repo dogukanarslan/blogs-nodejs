06def779f06437ef3c76cea56a4af8717080c667
dogukanarslan <dogukan2arslan@hotmail.com>
2021-02-08 19:28:03 +0300

    Add SASS middleware

--- app.js
+++ app.js
@@ -4,9 +4,25 @@ const blogRoutes = require('./routes/blogRoutes.js');
 const authRoutes = require('./routes/authRoutes.js');
 const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 const cookieParser = require('cookie-parser');
+const sassMiddleware = require('node-sass-middleware');
 
 // Create an express app
 const app = express();
+
+// Middleware and static files
+app.use(
+  sassMiddleware({
+    src: __dirname + '/scss',
+    dest: __dirname + '/public',
+    debug: true,
+    outputStyle: 'compressed'
+  })
+);
+
+app.use(express.json());
+app.use(express.static('public'));
+app.use(cookieParser());
+app.set('view engine', 'ejs');
 
 // Connect to mongodb
 const url = 'mongodb://127.0.0.1:27017/blogs';
@@ -14,13 +30,6 @@ mongoose
   .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(3000))
   .catch((err) => console.log(err));
-
-app.set('view engine', 'ejs');
-
-// Middleware and static files
-app.use(express.json());
-app.use(express.static('public'));
-app.use(cookieParser());
 
 // Check user for every request
 app.get('*', checkUser);
